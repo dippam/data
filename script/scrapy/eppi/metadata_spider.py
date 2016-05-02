1581d671bc52338ec4a9cbdbcbb30038a75448a7
Conail Stewart <conailstewart@gmail.com>
2016-05-03 01:20:32 +0200

    Iterate over full collection instead of first 100 items.

--- script/scrapy/eppi/metadata_spider.py
+++ script/scrapy/eppi/metadata_spider.py
@@ -8,7 +8,7 @@ class EppiSpider(scrapy.Spider):
     # Fetch directory of all EPPI documents -- 10292 in total.
     # A html partial is returned containing a JSON facet description and HTML document headers.
     # We ignore the JSON and iterate over the document headers.
-    start_urls = ['http://www.dippam.ac.uk/eppi/results?search%5Bper_page%5D=102&search%5Bpage%5D=1&search%5Btotal_pages%5D=&search%5Bview%5D=list&search%5Bqclean%5D=&search%5Bq%5D=%0A&search%5Bfields%5D=fulltext&search%5Bchrono%5D=on&search%5Bfrom%5D=1801&search%5Bto%5D=1922&search%5Bcat%5D%5B0%5D=on&search%5Bcat%5D%5B1%5D=on&search%5Bcat%5D%5B2%5D=on&search%5Bcat%5D%5B3%5D=on&search%5Bcat%5D%5B4%5D=on&search%5Bsort%5D=title&search%5Bsort_dir%5D=asc']
+    start_urls = ['http://www.dippam.ac.uk/eppi/results?search%5Bper_page%5D=10292&search%5Bpage%5D=1&search%5Btotal_pages%5D=&search%5Bview%5D=list&search%5Bqclean%5D=&search%5Bq%5D=%0A&search%5Bfields%5D=fulltext&search%5Bchrono%5D=on&search%5Bfrom%5D=1801&search%5Bto%5D=1922&search%5Bcat%5D%5B0%5D=on&search%5Bcat%5D%5B1%5D=on&search%5Bcat%5D%5B2%5D=on&search%5Bcat%5D%5B3%5D=on&search%5Bcat%5D%5B4%5D=on&search%5Bsort%5D=title&search%5Bsort_dir%5D=asc']
 
     # Parse the metadata directory.
     def parse(self, response):
